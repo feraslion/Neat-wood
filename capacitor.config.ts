4b5094d538be2a0774f30481a4d04852509e39ec
feraslion <75075323+feraslion@users.noreply.github.com>
2026-07-02 21:31:38 +0300

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yourcompany.neatwood',
+  appName: 'NeatWood',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
